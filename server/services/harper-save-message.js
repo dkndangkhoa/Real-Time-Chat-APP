e14cd4c7d7a9a7cdfa8c45eb0eb854599fa9dda2
DESKTOP-238LMAC\Khoa <k.nguyendangkhoa@student.fontys.nl>
2024-01-26 03:50:54 +0100

    DKN - Chat-App 1st commit

---
+++ server/services/harper-save-message.js
@@ -0,0 +1,39 @@
+var axios = require('axios');
+
+function harperSaveMessage(message, username, room) {
+  const dbUri = process.env.MONGO_URI;
+
+  var data = JSON.stringify({
+    operation: 'insert',
+    schema: 'realtime_chat_app',
+    table: 'messages',
+    records: [
+      {
+        message,
+        username,
+        room,
+      },
+    ],
+  });
+
+  var config = {
+    method: 'post',
+    url: dbUri,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: data,
+  };
+
+  return new Promise((resolve, reject) => {
+    axios(config)
+      .then(function (response) {
+        resolve(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        reject(error);
+      });
+  });
+}
+
+module.exports = harperSaveMessage;
